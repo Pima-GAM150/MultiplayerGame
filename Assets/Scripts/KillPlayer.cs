3c4626229609e9ffee7b962f35ee99997c504652
Cody <codycook33@gmail.com>
2019-03-06 13:46:08 -0700

    collision works properly

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+    	if (col.transform.tag == "Player" )
+    	{
+    		print("I speared the player.");
+    		Destroy();
+    	}
+    }
+    public void Destroy()
+    {
+    	Destroy(this);
+    }
+}
